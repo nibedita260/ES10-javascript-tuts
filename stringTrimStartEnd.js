b67da5f18bce44ddc7a329faffc1893e8380b9dd
nibedita260 <deepi.nibedita260@gmail.com>
2021-12-17 10:48:27 +0530

    [Nibedita]Added:stringTrimStartAndEnd in ES10 in Javascript Features Implemented

---
+++ stringTrimStartEnd.js
@@ -0,0 +1,9 @@
+//The trimStart() method removes whitespace from the beginning of a string.
+//The trimEnd() method removes whitespace from the end of a string.
+var greeting='  Hello Nibedita! ';
+
+console.log("greeting:"+greeting+",Hope you like it!");
+
+console.log("greeting:"+greeting.trimStart()+",Hope you like it!");
+
+console.log("greeting:"+greeting.trimEnd()+",Hope you like it!");
