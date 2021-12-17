cd22ed74068f895287f650264f891e3fc78b81f1
nibedita260 <deepi.nibedita260@gmail.com>
2021-12-17 10:55:25 +0530

    [Nibedita]Added:optional catchBinding in ES10 in Javascript Features Implemented

---
+++ optionalCatchBinding.js
@@ -0,0 +1,10 @@
+//earlier compulsory to give param in catach block
+
+var obj=[['name','nibi'],['age',25]]
+try{
+    console.log(Object.entries(objq))
+    console.log("Welcome TO RotaryClub!!")
+}
+catch{
+    console.log("Something went Wrong!!")
+}
